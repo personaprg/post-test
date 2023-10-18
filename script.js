fc70abde6b3123bfab035777323176ea87a5bc01
동현 <Garry@example.com>
2023-10-18 15:55:50 +0900

    외부 css(style), script(script) 파일 연결

---
+++ script.js
@@ -0,0 +1,12 @@
+ID = document.getElementById("ID");
+PASS1 = document.getElementById("PASS1");
+PASS2 = document.getElementById("PASS2");
+EMAIL = document.getElementById("EMAIL");
+
+
+if(ID && PASS1 && PASS2 && EMAIL !== ""){
+
+
+}else{
+  alert("값을 입력해주세요");
+}
