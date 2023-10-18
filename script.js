4358b3f0573e486359a3e65684ca2dd1c61b7cb9
동현 <Garry@example.com>
2023-10-18 16:31:55 +0900

    app.js(POST TEST) 구축, script(대소문자 판별) 작업

--- script.js
+++ script.js
@@ -1,11 +1,25 @@
-ID = document.getElementById("ID");
-PASS1 = document.getElementById("PASS1");
-PASS2 = document.getElementById("PASS2");
-EMAIL = document.getElementById("EMAIL");
+
+ID = document.getElementById("ID"); //아이디
+PASS1 = document.getElementById("PASS1"); // 페스워드
+PASS2 = document.getElementById("PASS2"); // 페스워드 확인
+EMAIL = document.getElementById("EMAIL"); // 이메일
+signUp = docuem.getElementById("signUp"); // 로그인 버튼
 
 
-if(ID && PASS1 && PASS2 && EMAIL !== ""){
 
+ID.value.forEach(element => {
+});
+
+
+
+for(let i = 0; i < array1.lenth; i++){
+
+
+}
+
+
+if(ID.value && PASS1.value && PASS2.value && EMAIL.value !== ""){
+  ID.value
 
 }else{
   alert("값을 입력해주세요");
