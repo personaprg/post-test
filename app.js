20b954c0bec8e5d27bbcc16f635ea9605d033639
동현 <Garry@example.com>
2023-10-19 09:53:08 +0900

    콘텐츠의 타입 변환을 위해 contentType 작성

--- app.js
+++ app.js
@@ -2,11 +2,12 @@
 const fs = require('fs');
 // import hyper text
 const http = require('http');
+const contentType = require("contentType");
 
 
-let serve = http.createServer(req, res, ()=>{
-
-  
 
 
-})
+let serve = http.createServer((req, res) => {
+  if(req === METHODS)
+
+});
