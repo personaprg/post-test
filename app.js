56dc2953c29795705846a7859e0edf47cbd7bdf2
동현 <Garry@example.com>
2023-10-19 13:53:20 +0900

    "post" 요청에 따른 처리

--- app.js
+++ app.js
@@ -2,27 +2,50 @@
 const fs = require('fs');
 // import hyper text
 const http = require('http');
-const contentType = require("./contentType");
+const queryString = require('querystring');
+const contentType = require('contentType');
 
+let main = "./index.html"
+let sub = "./info.html"
 
-let serve = http.createServer((req, res) => {
-  if(req.method === "GET" && req.url === "/index.html"){
-    fs.readFile("index", "UTF8",(err, data)=>{
-      if(err){
-        console.log(`서버 연결 실패! : ${err}`)
-      }else{
-        console.log("서버 작동중");
+// createServer 
+let server = http.createServer((req,res)=> {
+  if(req.method === 'GET' && req.url === '/'){
+    fs.readFile(main, 'utf8', (err, data)=> {
+      if(err) {
+        console.log(err);
+      } else {
         res.writeHead(200, contentType);
         res.end(data);
+      };
+    });  
+  } else if (req.method=== 'POST' && req.url === "/info.html"){
+    let body ='';
+    req.on('data', (chunk) =>{
+      body += chunk.toString();
+    })
+
+    req.on('end', () => {
+      const parsedBody = queryString.parse(body);
+      const {username, password} = parsedBody;
+
+      // check 
+      console.log(`form 입력으로부터 받은 데이터 확인 ->`, parsedBody);
+      console.log(`form 입력으로부터 받은 데이터 확인 ->`, username);
+      console.log(`form 입력으로부터 받은 데이터 확인 ->`, password);
+    })
+    fs.readFile(sub, 'utf8', (err, data)=> {
+      if(err) {
+        console.log(err);
+      } else {
+        res.writeHead(200, contentType)
+        res.end(data)
       }
     })
-  }else if(req.method === "POST" && req.url === '/index.html'){
-    let body = '';
-    req.on('data', (chunk) => {
-      body += chunk.tostring();
-
-    })
-
-
   }
 });
+
+let port = 1004
+server.listen(port, () => {
+  console.log(`http://localhost:${port}`);
+})
