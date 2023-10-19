11238655bad74c7936b20981ac1c76dc5136da7f
동현 <Garry@example.com>
2023-10-19 14:47:31 +0900

    로컬 디렉토리 정리(module, css) 폴더로 구분

--- app.js
+++ app.js
@@ -5,12 +5,14 @@ const http = require('http');
 const queryString = require('querystring');
 const contentType = require('./contentType');
 
-let main = "./index.html"
-let sub = "./info.html"
+let main = "./module/index.html"
+let sub = "./module/info.html"
+let style = "./module/style.css"
 
 // createServer 
 let server = http.createServer((req,res)=> {
   if(req.method === 'GET' && req.url === '/'){
+    //index.html
     fs.readFile(main, 'utf8', (err, data)=> {
       if(err) {
         console.log(err);
@@ -19,6 +21,18 @@ let server = http.createServer((req,res)=> {
         res.end(data);
       };
     });  
+
+    //style.css
+    fs.readFile(style, 'utf8', (err, data)=> {
+      if(err) {
+        console.log(err);
+      } else {
+        res.writeHead(200, contentType);
+        res.end(data);
+      };
+    }); 
+
+
   } else if (req.method=== 'POST' && req.url === "/info.html"){
     console.log("POST 요청 완료")
     let body ='';
