f96d118f68cd57012f543eda5628ffb883a4fb18
동현 <Garry@example.com>
2023-10-19 11:39:13 +0900

    "POST" 요청으로 받고 String 타입으로 변환하여 on 메소드 실행

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 const fs = require('fs');
 // import hyper text
 const http = require('http');
-const contentType = require("./contentType.js");
+const contentType = require("./contentType");
 
 
 let serve = http.createServer((req, res) => {
@@ -11,9 +11,18 @@ let serve = http.createServer((req, res) => {
       if(err){
         console.log(`서버 연결 실패! : ${err}`)
       }else{
+        console.log("서버 작동중");
         res.writeHead(200, contentType);
         res.end(data);
       }
     })
+  }else if(req.method === "POST" && req.url === '/index.html'){
+    let body = '';
+    req.on('data', (chunk) => {
+      body += chunk.tostring();
+
+    })
+
+
   }
 });
