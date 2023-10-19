b3f00e60adf1d425c2635927ad0e576d077877ff
동현 <Garry@example.com>
2023-10-19 10:11:33 +0900

    파일(index.thml)을 읽어와서 data를 전송하기 위해 fs 모듈 사용

--- app.js
+++ app.js
@@ -7,7 +7,13 @@ const contentType = require("./contentType.js");
 
 let serve = http.createServer((req, res) => {
   if(req.method === "GET" && req.url === "/index.html"){
-
-
+    fs.readFile("index", "UTF8",(err, data)=>{
+      if(err){
+        console.log(`서버 연결 실패! : ${err}`)
+      }else{
+        res.writeHead(200, contentType);
+        res.end(data);
+      }
+    })
   }
 });
